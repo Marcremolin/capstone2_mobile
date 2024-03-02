22880db9ed51d8a5be9bd984da91e1f16f21f243
Clarise Annely <clariseduco@gmail.com>
2024-03-02 21:42:05 +0800

    BACKEND WORKING _ Upload Proof of Emergency

--- server/controller/emergency.controller.js
+++ server/controller/emergency.controller.js
@@ -1,4 +1,5 @@
 const Emergency = require("../services/emergency.services");
+
 exports.createEmergencySignal = async (req, res, next) => {
   try {
     console.log('Request Body:', req.body); 
@@ -14,15 +15,8 @@ exports.createEmergencySignal = async (req, res, next) => {
       status
     } = req.body;
 
-    const cloudinaryResponse = req.file; // Access the Cloudinary response from req.file
-
-    // Extract URL and public ID from the Cloudinary response
-    const emergencyProofImage = {
-      url: cloudinaryResponse.secure_url,
-      public_id: cloudinaryResponse.public_id
-    };
-
-    let emergencyReq = await Emergency.createEmergencySignal(
+    // Pass the file to the service function
+    const savedEmergencySignal = await Emergency.createEmergencySignal(
       userId,
       residentName,
       currentLocation,
@@ -30,13 +24,57 @@ exports.createEmergencySignal = async (req, res, next) => {
       emergencyType,
       date,
       status,
-      emergencyProofImage // Pass emergencyProofImage to the service function
+      req.file // Pass the file directly
     );
 
-    console.log('Emergency request created:', emergencyReq);
+    console.log('Emergency request created:', savedEmergencySignal);
 
-    res.json({ status: true, success: emergencyReq });
+    res.json({ status: true, success: savedEmergencySignal });
   } catch (error) {
     next(error);
   }
 }
+
+
+// const Emergency = require("../services/emergency.services");
+// exports.createEmergencySignal = async (req, res, next) => {
+//   try {
+//     console.log('Request Body:', req.body); 
+//     console.log('Uploaded File:', req.file); // Check if the file is correctly received
+
+//     const {
+//       userId,
+//       residentName,
+//       currentLocation,
+//       phoneNumber,
+//       emergencyType,
+//       date,
+//       status
+//     } = req.body;
+
+//     const cloudinaryResponse = req.file; // Access the Cloudinary response from req.file
+
+//     // Extract URL and public ID from the Cloudinary response
+//     const emergencyProofImage = {
+//       url: cloudinaryResponse.secure_url,
+//       public_id: cloudinaryResponse.public_id
+//     };
+
+//     let emergencyReq = await Emergency.createEmergencySignal(
+//       userId,
+//       residentName,
+//       currentLocation,
+//       phoneNumber,
+//       emergencyType,
+//       date,
+//       status,
+//       emergencyProofImage // Pass emergencyProofImage to the service function
+//     );
+
+//     console.log('Emergency request created:', emergencyReq);
+
+//     res.json({ status: true, success: emergencyReq });
+//   } catch (error) {
+//     next(error);
+//   }
+// }
