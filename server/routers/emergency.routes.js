ed8a86fd8b7be4eee0b78457d99c39438168179e
Clarise Annely <clariseduco@gmail.com>
2024-03-02 15:03:45 +0800

    TEST 1

--- server/routers/emergency.routes.js
+++ server/routers/emergency.routes.js
@@ -2,19 +2,17 @@ const router = require('express').Router();
 const multer = require('multer');
 const EmergencyController = require("../controller/emergency.controller");
 
-// Multer configuration for handling file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/emergency'); // Specify the directory where files should be uploaded
+    cb(null, 'uploads/emergency'); 
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original filename for storing the file
+    cb(null, file.originalname); 
   }
 });
 
 const upload = multer({ storage: storage });
 
-// Route handler for creating emergency signal
 router.post('/emergencySignal', upload.single('emergencyProofImage'), EmergencyController.createEmergencySignal);
 
 module.exports = router;
